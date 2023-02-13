10ca874af2bc1f436bfb25ba7b7b8c24cbf77e3b
jakubsfdc <jszelagowski@salesforce.com>
2023-02-13 18:18:20 +0000

    #15 fixes the logical statement that catches actions other than Next and Finish

--- force-app/main/default/lwc/govNavigationButtons/govNavigationButtons.js
+++ force-app/main/default/lwc/govNavigationButtons/govNavigationButtons.js
@@ -1,7 +1,7 @@
 /**
  * Component Name: Gov UK Navigation Buttons
  * Derived_From_Frontend_Version:v3.13.1
- * Created by: Simon Cook Updated by Harshpreet Singh Chhabra/Brenda Campbell
+ * Created by: Simon Cook Updated by Harshpreet Singh Chhabra/Brenda Campbell, Jakub Szelagowski
  **/
 import {LightningElement, api, track, wire} from 'lwc';
 import { FlowNavigationBackEvent, FlowNavigationNextEvent, FlowNavigationFinishEvent } from 'lightning/flowSupport';
@@ -41,7 +41,7 @@ export default class GovNavigationButtons extends LightningElement {
 
     connectedCallback() {
         // subscribe to registration events
-        this.subscribeMCs()
+        this.subscribeMCs();
 
         // bug out if no configuration string
         if(!this.buttonActionsString ||
@@ -54,6 +54,7 @@ export default class GovNavigationButtons extends LightningElement {
         // create the object base on the csv
         const buttonAlignments = this.buttonAlignmentsString.split(',');
         const buttonLabels = this.buttonLabelsString.split(',');
+
         const buttonActions = this.buttonActionsString.split(',');
         const buttonVariant = this.buttonVariantsString.split(',');
 
@@ -101,14 +102,25 @@ export default class GovNavigationButtons extends LightningElement {
         return (this.fullWidth) ? "" : "govuk-grid-column-two-thirds";
     }
 
+
+    
+
     // Event handlers functions
     handleClick(event) {
         // get the action for the data-action attribute
+        //var elementToSelect = null;
         this.action = event.target.getAttribute('data-action').toUpperCase();
+
+            // console.log('====================== ');
+            // console.log('this.action:' + this.action);
+            // console.log('this.availableActions' + this.availableActions);
+            // console.log('this.components' + this.components);
+
         // check to see if next or finish was selected and we have components to validate
         if( (this.action === 'NEXT' || this.action === 'FINISH') && this.components.length > 0 ) {
             this.components.forEach(component => {
                 component.isValid = false;
+                //this.focusOnErroBox();
             })
             //console.log('NAVIGATION_BUTTONS: Sending validation message ' + this.fieldId );
             publish(this.messageContext, VALIDATE_MC, { componentId: this.fieldId });
@@ -129,6 +141,11 @@ export default class GovNavigationButtons extends LightningElement {
         } else if (this.action === 'BACK' &&
             this.availableActions.find(action => action === 'BACK')) {
             const event = new FlowNavigationBackEvent();
+            this.dispatchEvent(event);
+        } else {
+            // console.log('====================== ');
+            // console.log('this.action:' + this.action);
+            const event = new FlowNavigationNextEvent();
             this.dispatchEvent(event);
         }
     }
