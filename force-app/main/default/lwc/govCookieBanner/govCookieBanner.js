627e4c9366701cc24a1d1a4c9326f0cfa258e083
John Davies <johndavies1968@gmail.com>
2023-02-23 11:29:51 +0000

    cookie manager

--- force-app/main/default/lwc/govCookieBanner/govCookieBanner.js
+++ force-app/main/default/lwc/govCookieBanner/govCookieBanner.js
@@ -1,12 +1,31 @@
 import { LightningElement, api } from 'lwc';
-import { NavigationMixin } from 'lightning/navigation';
+import { publish, createMessageContext, MessageContext } from 'lightning/messageService';
+import COOKIES_ACCEPT_CHANNEL from '@salesforce/messageChannel/CookiesAccept__c';
 
-export default class GovCookieBanner extends  NavigationMixin(LightningElement) {
+export default class GovCookieBanner extends  LightningElement {
 
     @api label;
     @api isVisible;
     @api cookiePageUrl;
+    @api acceptLabel;
+    @api rejectLabel;
+    @api bannerContent;
 
-    
+    messageContext = createMessageContext();
 
+    accept() {
+        this.sendAcceptRecommendedMessage(true);
+        
+    }
+
+    reject() {
+        this.sendAcceptRecommendedMessage(false);
+    }
+
+    sendAcceptRecommendedMessage(accept){
+        const payload = { 
+            acceptRecommended: accept
+          };
+          publish(this.messageContext, COOKIES_ACCEPT_CHANNEL, payload);
+    }
 }
