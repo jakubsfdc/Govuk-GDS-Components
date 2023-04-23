be59729b580713b7ef42dcac7048fdd650346754
jakubsfdc <jszelagowski@salesforce.com>
2023-04-23 10:56:28 +0100

    #50 cleaned up and consilidated code

--- force-app/main/default/lwc/govErrorMessages/govErrorMessages.js
+++ force-app/main/default/lwc/govErrorMessages/govErrorMessages.js
@@ -24,18 +24,13 @@ export default class ErrorMessages extends LightningElement {
     handleLinkBlur() {
         this.highlightedLinkClass = '';
     }
-    ///
 
     components = [];
-
-    componentTypeName = "C-GOV-ERROR-MESSAGE";
-
     errorPrefix = Error_summary_title;
 
     // messaging attributes
     @wire(MessageContext) messageContext;
     errorSubscription;
-
      
     connectedCallback() {
         this.subscribeMCs();
@@ -46,13 +41,10 @@ export default class ErrorMessages extends LightningElement {
     }
 
     get hasErrors() {
-        //console.log('inside hasErrors');
-        // console('this.components::. '+this.components);
         return (this.components.filter(component => component.isValid === false).length > 0);
     }
 
     subscribeMCs() {
-        // console.log('inside subscribeMCs')
         if (this.errorSubscription) {
             return;
         }
@@ -77,101 +69,28 @@ export default class ErrorMessages extends LightningElement {
     }
 
     putFocusOnError(){
-        
-        
-        //const myComponent = this.template.querySelector('div[class="govuk-width-container"]');
         const myComponent = this.template.querySelector('a[name="errorSummaryTitle"]');
         myComponent.focus();
-
-        
-        //const myComponentBox = this.template.querySelector('div[class="govuk-width-container"]');
-//         const myComponentBox = this.template.querySelector('div[class="govuk-error-summary error-summary"]');
-        
-        // const myComponentBox = this.template.querySelector(`[data-module="govuk-error-summary"]`);
-        // console.log('myComponentBox: '+ myComponentBox);
-        // // myComponentBox.
-        // myComponentBox.focus();
-
-
-        // let myComponentDivBox = this.template.querySelector('div[class="govuk-width-container"]'); //.getElementById(this.textFieldId);
-        // console.log('myCompId: '+ myCompId)
-        
-        // myComponentDivBox.addEventListener("focus", function(){
-        //     this.classList.add("highlighted-link");
-        // });
-
-        // myComponentDivBox.addEventListener("blur", function(){
-        //     this.classList.remove("highlighted-link");
-        // });
-        
-
-
-         // console.log('inside handleValidationStateMessage');
-// let myLink = this.template.querySelector('a[name="errorSummaryTitle"]');
-// // console.log('Set up focus on a# myLink::. '+myLink);
-// myLink.focus();
-         
-        //setTimeout(function(){
-            // let myAnchor0 = this.template.querySelector(this.components[0].id);
-            // myAnchor0.focus();
-             // console.log('Set up focus on components'+ this.components);
-            if (this.components && this.components.length>0){
-                 // console.log('Set up focus on component id'+ this.components[0].id);
-
-                // let myLinkA = this.template.getElementById(this.components[0].id);
-                // myLinkA.focus();
-
-                // this.template.getElementById(this.components[0].id).addEventListener("focus", function(){
-                //     this.classList.add("highlighted-link");
-                // });
-        
-                // this.template.getElementById(this.components[0].id).addEventListener("blur", function(){
-                //     this.classList.remove("highlighted-link");
-                // });
-
-              
-                
-            }
     }
     // called during validation to update error states and messages
     handleValidationStateMessage(message) {
-        // console.log('### message.componentId ::.'+message.componentId);
-        // console.log('### message.isValid ::.'+message.isValid);
-        // console.log('### message.error ::.'+message.error);
-        // console.log('### message.componentType ::.'+message.componentType);
-        // console.log('### message.componentSelect ::.'+message.componentSelect);
-
         const component = this.components.find(component => component.id === message.componentId);
         
         if(component) {
-            // console.log('### component is not null');
             if(message.isValid === true) {
-                // console.log('### message.isValid is true');
                 this.components = this.components.filter(component => component.id !== message.componentId);
             } else {
-                // console.log('### message.isValid is false.');
-                // console.log('### Setting component.isValid to '+ message.isValid);
-                // console.log('### Setting component.error to '+ message.error);
                 component.isValid = message.isValid;
                 component.error = message.error;
             }
         } else {
-            // console.log('### component IS null');
             if(message.isValid === false) {
-                // console.log('message.isValid = ::. '+ message.isValid);
                 const component = {};
                 component.id = message.componentId;
                 component.isValid = message.isValid;
                 component.error = message.error;
                 component.componentType = message.componentType;
                 component.componentSelect = message.componentSelect;
-                // console.log('Component to push to array: ' + component);
-                // console.log(component.id);
-                // console.log(component.isValid);
-                // console.log(component.error);
-                // console.log(component.componentType);
-                // console.log(component.componentSelect);
-                // console.log('=========== // ');
                 this.components.push(component);
             }
         }
@@ -179,29 +98,12 @@ export default class ErrorMessages extends LightningElement {
 
     // called at the start of validation to remove existing errors
     handleValidateMessage(message) {
-        console.log('inside handleValidateMessage');
         this.putFocusOnError();
         this.components = [];
     }
 
     handleClick(event) {
-          console.log('govErrorMessages.js');
-          console.log('//////////////////// handleClick:');
-
           let targetId = event.target.dataset.targetId; // Component id, i.e. input-text-18, date-input-day-47, date-input-month-47
-          console.log('targetId:' + targetId);
-         
-          console.log('about to call handleInputFieldFocus('+targetId+')');
-          this.handleInputFieldFocus(targetId);
-          console.log('handleInputFieldFocus('+targetId+') called');
-    }
-
-    handleInputFieldFocus(fieldId) {
-        console.log('^^^^^^^^^ ');
-        console.log('HELLOOOOOOO from govErrorMessage.handleInputFieldFocus after click!');
-        console.log('inside handleInputFieldFocus + ' + fieldId)
-            publish(this.messageContext, SET_FOCUS_MC, { componentId: fieldId });
-        console.log('event dispatched to component with id. over and out.');
-        console.log('^ ');
+          publish(this.messageContext, SET_FOCUS_MC, { componentId: targetId });
     }
 }
