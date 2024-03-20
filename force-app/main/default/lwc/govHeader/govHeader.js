2e66ab326fe935814e9ca612a5bfd54ae75f85c0
jakubsfdc <jszelagowski@salesforce.com>
2024-03-20 11:12:13 +0000

    Removed debug/comments

--- force-app/main/default/lwc/govHeader/govHeader.js
+++ force-app/main/default/lwc/govHeader/govHeader.js
@@ -1,7 +1,7 @@
 /**
  * Component Name: Gov UK Header
  * Derived_From_Frontend_Version:v3.13.1
- * Created by: Simon Cook Updated by Neetesh Jain/Brenda Campbell
+ * Created by: Simon Cook Updated by Neetesh Jain/Brenda Campbell Jakub Szelagowski
  **/
 import {LightningElement, api, track} from 'lwc';
 import {NavigationMixin} from "lightning/navigation";
@@ -35,10 +35,6 @@ export default class GovHeader extends NavigationMixin(LightningElement) {
             strNavigationMenuDevName: this.navigationMenuDevName
         })
             .then(menuItems => {
-                console.log("*** menuItems *** ");
-                console.log("menuItems: " + menuItems);
-                console.log("*** ");
-
                 try {
                     // get the page title
                     let urlParts = window.location.href.split("/");
