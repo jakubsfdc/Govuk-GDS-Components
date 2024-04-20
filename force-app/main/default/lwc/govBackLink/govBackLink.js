e9a127cce3ab1ef2daded5e14b96a0b0f91946b8
jakubsfdc <jszelagowski@salesforce.com>
2024-04-20 09:57:59 +0100

    Fixes #86 setting tab index to -1 in template and adding listener Tab to set index to 0

--- force-app/main/default/lwc/govBackLink/govBackLink.js
+++ force-app/main/default/lwc/govBackLink/govBackLink.js
@@ -1,7 +1,7 @@
 /**
  * Component Name: Gov UK Back Link
  * Derived_From_Frontend_Version:v3.13.1
- * Created by: Simon Cook Updated by Neetesh Jain/Brenda Campbell
+ * Created by: Simon Cook Updated by Neetesh Jain/Brenda Campbell, Jakub Szelagowski
  **/
 import { LightningElement, api } from 'lwc';
 import { FlowNavigationBackEvent } from 'lightning/flowSupport';
@@ -20,6 +20,42 @@ export default class GovBackLink extends NavigationMixin(LightningElement) {
         if (this.availableActions.find(action => action === 'BACK')) {
             this.backAvailable = true;
         }
+    }
+
+    setBacklinkAttributeToZero(){
+        console.log("renderedCallBack");
+        var codes = this.template.querySelector('.govuk-back-link')
+        console.log('*** codes: ' + codes);
+        console.dir(codes);
+        console.log('*** after dir *** ');
+            
+
+        // set tabindex of codes to 0 to make it accessible by tabbing
+        if (codes) {
+            codes.setAttribute('tabindex', '0');
+        }
+        console.log('*** after setAttribute *** ');
+        console.dir(codes);
+        
+        console.log('*** after setAttribute tabindex dir *** ');
+         // console.log('Index set to 0 on govuk-back-link:' + codes[0].getAttribute['tabindex']);
+    }
+
+    renderedCallback(){
+
+        // listen for Tab button being pressed on keyboard for BackLink
+        // TODO: may have to remove the listener at some point so it doesn’t fire on every tab
+        document.addEventListener('keydown', (event) => { 
+            if (event.key === 'Tab') {
+                console.log('*** TAB Pressed *** ');
+                this.setBacklinkAttributeToZero();
+                console.log('*** after setBacklinkAttributeToZero run *** ');
+            }
+        });
+         
+
+       
+       
     }
 
     handleBack(event) {
